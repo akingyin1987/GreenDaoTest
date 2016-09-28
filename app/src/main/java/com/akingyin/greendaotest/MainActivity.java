5ffa7355d44f083c873cbf8d26f36d10d9034eb5
akingyin1987 <akingyin@163.com>
2016-09-28 14:14:37 +0800

    greendao

--- app/src/main/java/com/akingyin/greendaotest/MainActivity.java
+++ app/src/main/java/com/akingyin/greendaotest/MainActivity.java
@@ -5,12 +5,15 @@ import android.content.ContentValues;
 import android.database.Cursor;
 import android.net.Uri;
 import android.provider.BaseColumns;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,34 +21,58 @@ public class MainActivity extends AppCompatActivity {
   ContentResolver contentResolver;
   DemoAdapter  adapter;
   RecyclerView  recyclerview;
+  FloatingActionButton  fab_autor;
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
     recyclerview = (RecyclerView)findViewById(R.id.recyclerview);
-
+    fab_autor = (FloatingActionButton)findViewById(R.id.fab_autor);
     if(null == contentResolver ){
       contentResolver = getContentResolver();
     }
 
-    Cursor cursor = contentResolver.query(contentUri, null, null, null, null);
-    List<User> data = new ArrayList<>();
-    if(null != cursor){
-      System.out.println("count="+cursor.getCount());
-      while (cursor.moveToNext()){
-        User  userEntity = new User();
-        userEntity.arg = cursor.getString(cursor.getColumnIndex("AGE"));
-        userEntity.account = cursor.getString(cursor.getColumnIndex("ACCOUNT"));
-
-        userEntity.setId(cursor.getLong(cursor.getColumnIndex(BaseColumns._ID)));
-        data.add(userEntity);
+    fab_autor.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View v) {
+        List<User> data = new ArrayList<>();
+        queryData(data);
+        adapter.getData().clear();
+        adapter.addData(data);
+        adapter.notifyDataSetChanged();
       }
-      cursor.close();
-    }
+    });
+    List<User> data = new ArrayList<>();
+    queryData(data);
+
     adapter = new DemoAdapter(R.layout.item_view,data);
     recyclerview.setItemAnimator(new DefaultItemAnimator());
     recyclerview.setLayoutManager(new LinearLayoutManager(this));
     recyclerview.setAdapter(adapter);
-    System.out.println("data.size="+data.size());
 
+
+  }
+
+  public   void   queryData(List<User> data){
+    Cursor cursor = contentResolver.query(contentUri, null, null, null, null);
+    System.out.println("null == cursor"+(null == cursor));
+    if(null != cursor){
+
+      if(cursor.moveToFirst()){
+        String[]  columns = cursor.getColumnNames();
+
+        while (cursor.moveToNext()){
+          User  userEntity = new User();
+
+          userEntity.arg = cursor.getString(cursor.getColumnIndex("ARG"));
+
+          userEntity.account = cursor.getString(cursor.getColumnIndex("ACCOUNT"));
+
+          userEntity.uuid = cursor.getString(cursor.getColumnIndex("UUID"));
+
+          userEntity.setId(cursor.getLong(cursor.getColumnIndex(BaseColumns._ID)));
+          data.add(userEntity);
+        }
+        cursor.close();
+      }
+    }
   }
 }
